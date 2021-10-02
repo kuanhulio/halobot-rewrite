253e1ca3a50ee2bb3c8e3a3525a67ae2835e088d
Sami Altamimi <saltamimi@kuantum.link>
2021-10-02 00:39:05 -0500

    cogs/mcc_stats.py: get ready for squads
    We're adding another table to the database. :)
    
    Pipfile: add dependencies then remove them

--- cogs/mcc_stats.py
+++ cogs/mcc_stats.py
@@ -1,9 +1,6 @@
-import requests
 import discord
-import json
 import datetime
 import aiohttp
-import asyncio
 
 from pony.orm import *
 from os.path import exists
@@ -39,12 +36,20 @@ class Gamer(db.Entity):
     discord_id = Required(str)
     gamertag   = Required(str)
 
-if exists('xboxusers.sqlite') is False:
-    db.bind(provider='sqlite', filename='xboxusers.sqlite', create_db=True)
-    db.generate_mapping(create_tables=True)
+class Squads(db.Entity):
+    squad_name  = Required(str)
+    owner       = Required(Gamer)
+    coowner     = Optional(str)
+    members     = Optional(StrArray)
+    channel_ids = Required(StrArray)
+    role_ids    = Required(StrArray)
 
+
+db.bind(provider='sqlite', filename='xboxusers.sqlite', create_db=True)
+db.generate_mapping(create_tables=True)
 
 class MCCStats(commands.Cog):
+    """Get your Spartan Stats from the Halo: The Master Chief Collection"""
     def __init__(self, bot):
         self.bot = bot
     
@@ -114,55 +119,6 @@ class MCCStats(commands.Cog):
                 embed.add_field(name="Deaths Per Game", value=response["deathsPerGame"], inline=True)
                 embed.add_field(name="Current Streak", value=response["streak"], inline=True)
                 await ctx.send(embed=embed)
-
-
-    # @commands.command()
-    # async def latestgame(self, ctx, *, gamertag = None, game="All"):
-    #     """ Grab latest game stats from a spartan's service record """
-    #     FORMED_URL = BASE_ENDPOINT + GAMES_LATEST_ENDPOINT
-    #     SHOULD_CONTINUE = True
-
-    #     if game != "All" and game not in CORRECT_GAMES:
-    #         await ctx.send("You'll need to use the correct game. Ex: h1, h2, h3, hr...")
-    #         SHOULD_CONTINUE = False
-        
-    #     if gamertag is None:
-    #         with open("user_to_xbox.json", "r") as f:
-    #             gamertags = json.load(f)
-
-    #         try:
-    #             gamertag = gamertags[f"{ctx.author.id}"]
-    #         except KeyError:
-    #             await ctx.send("Your Gamertag is not set! Do it with the `/setxbox <your_gamertag>` command or give me a gamertag to use!")
-    #             SHOULD_CONTINUE = False
-    #     else:
-    #         gamertag = gamertag
-
-    #     if SHOULD_CONTINUE:
-    #         payload = {"gamertag": gamertag, "game": game}
-    #         response = requests.post(FORMED_URL, payload)
-
-    #         embed = discord.Embed(
-    #             title="Latest Game Stats for Spartan {}".format(gamertag), 
-    #             colour=discord.Colour(0x40cf0f), 
-    #             timestamp=datetime.datetime.utcfromtimestamp(datetime.datetime.now().timestamp())
-    #         )
-
-    #         embed.set_footer(text=f"Requested by {ctx.author.display_name}")
-
-    #         embed.add_field(name="Game Variant", value=response["games"][0]["gameVariant"], inline=True)
-    #         embed.add_field(name="Map ID", value=response["games"][0]["mapId"], inline=True)
-    #         embed.add_field(name="Won?", value=response["games"][0]["won"], inline=True)
-    #         embed.add_field(name="Score", value=response["games"][0]["score"], inline=True)
-    #         embed.add_field(name="Assists", value=response["games"][0]["assists"], inline=True)
-    #         embed.add_field(name="Kills", value=response["games"][0]["kills"], inline=True)
-    #         embed.add_field(name="Deaths", value=response["games"][0]["deaths"], inline=True)
-    #         embed.add_field(name="Kill/Death Ratio", value=response["games"][0]["killDeathRatio"], inline=True)
-    #         embed.add_field(name="Headshot Rate", value=response["games"][0]["headshotRate"], inline=True)
-    #         embed.add_field(name="Played at...", value=response["games"][0]["playedAtRecency"], inline=True)
-    #         embed.add_field(name="Headshots", value=response["games"][0]["headshots"], inline=True)
-    #         embed.add_field(name="Medals", value=response["games"][0]["medals"], inline=True)        
-    #         await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(MCCStats(bot))
