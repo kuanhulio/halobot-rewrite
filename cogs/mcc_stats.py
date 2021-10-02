a4a9de91d1dd99aa3df27f17833af6ca0cea0114
Sami Altamimi <saltamimi@kuantum.link>
2021-10-01 20:09:36 -0500

    cogs/mcc_stats: add the stats command
    This command reads from the MCC API and provides stats.

--- cogs/mcc_stats.py
+++ cogs/mcc_stats.py
@@ -77,49 +77,44 @@ class MCCStats(commands.Cog):
                 await ctx.send(f"I've set your gamertag to `{gamertag}`!")
         
 
-    # @commands.command()
-    # async def stats(self, ctx, *, gamertag = None):
-    #     """ Grab stats about a Spartan """
-    #     SHOULD_CONTINUE = True
-    #     if gamertag is None:
-    #         with open("user_to_xbox.json", "r") as f:
-    #             gamertags = json.load(f)
+    @commands.command()
+    async def stats(self, ctx, *, gamertag = None):
+        """ Grab stats about a Spartan """
 
-    #         try:
-    #             gamertag = gamertags[f"{ctx.author.id}"]
-    #         except KeyError:
-    #             await ctx.send("Your Gamertag is not set! Do it with the `/setxbox <your_gamertag>` command or give me a gamertag to use!")
-    #             SHOULD_CONTINUE = False
-    #     else:
-    #         gamertag = gamertag
-        
-    #     if SHOULD_CONTINUE:
-    #         FORMED_URL = BASE_ENDPOINT + STATS_ENDPOINT
-    #         payload = {"gamertag": gamertag}
-    #         response = requests.post(FORMED_URL, payload)
+        # When working with the database, we have to have this context manager.
+        with db_session:
+            if len(select(g for g in Gamer if g.discord_id == str(ctx.author.id))[:]) == 0:
+                await ctx.send("You haven't linked your Gamertag. Use `/setxbox <gamertag>`.")
+            else:
+                params = {"gamertag": gamertag}
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(BASE_ENDPOINT + STATS_ENDPOINT, params=params) as resp:
+                        response = await resp.json()
+                    await session.close()
+                    
+                embed = discord.Embed(
+                    title=f"Stats for Spartan {gamertag}", 
+                    colour=discord.Colour(0x40cf0f), 
+                    timestamp=datetime.datetime.utcfromtimestamp(datetime.datetime.now().timestamp())
+                )
 
-    #         embed = discord.Embed(
-    #             title="Stats for Spartan {}".format(gamertag), 
-    #             colour=discord.Colour(0x40cf0f), 
-    #             timestamp=datetime.datetime.utcfromtimestamp(datetime.datetime.now().timestamp())
-    #         )
+                embed.set_thumbnail(url=response["emblem"])
+                embed.set_footer(text=f"Requested by {ctx.author.display_name}")
 
-    #         embed.set_thumbnail(url=response.json()["emblem"])
-    #         embed.set_footer(text=f"Requested by {ctx.author.display_name}")
+                embed.add_field(name="Clantag", value=response["clantag"], inline=True)
+                embed.add_field(name="Playtime", value=response["playtime"], inline=True)
+                embed.add_field(name="Games Played", value=response["gamesPlayed"], inline=True)
+                embed.add_field(name="Wins", value=response["wins"], inline=True)
+                embed.add_field(name="Losses", value=response["losses"], inline=True)
+                embed.add_field(name="Win Ratio", value=response["winRatio"], inline=True)
+                embed.add_field(name="Kills", value=response["kills"], inline=True)
+                embed.add_field(name="Deaths", value=response["deaths"], inline=True)
+                embed.add_field(name="Kill/Death Ratio", value=response["killDeathRatio"], inline=True)
+                embed.add_field(name="Kills Per Game", value=response["killsPerGame"], inline=True)
+                embed.add_field(name="Deaths Per Game", value=response["deathsPerGame"], inline=True)
+                embed.add_field(name="Current Streak", value=response["streak"], inline=True)
+                await ctx.send(embed=embed)
 
-    #         embed.add_field(name="Clantag", value=response.json()["clantag"], inline=True)
-    #         embed.add_field(name="Playtime", value=response.json()["playtime"], inline=True)
-    #         embed.add_field(name="Games Played", value=response.json()["gamesPlayed"], inline=True)
-    #         embed.add_field(name="Wins", value=response.json()["wins"], inline=True)
-    #         embed.add_field(name="Losses", value=response.json()["losses"], inline=True)
-    #         embed.add_field(name="Win Ratio", value=response.json()["winRatio"], inline=True)
-    #         embed.add_field(name="Kills", value=response.json()["kills"], inline=True)
-    #         embed.add_field(name="Deaths", value=response.json()["deaths"], inline=True)
-    #         embed.add_field(name="Kill/Death Ratio", value=response.json()["killDeathRatio"], inline=True)
-    #         embed.add_field(name="Kills Per Game", value=response.json()["killsPerGame"], inline=True)
-    #         embed.add_field(name="Deaths Per Game", value=response.json()["deathsPerGame"], inline=True)
-    #         embed.add_field(name="Current Streak", value=response.json()["streak"], inline=True)
-    #         await ctx.send(embed=embed)
 
     # @commands.command()
     # async def latestgame(self, ctx, *, gamertag = None, game="All"):
@@ -155,18 +150,18 @@ class MCCStats(commands.Cog):
 
     #         embed.set_footer(text=f"Requested by {ctx.author.display_name}")
 
-    #         embed.add_field(name="Game Variant", value=response.json()["games"][0]["gameVariant"], inline=True)
-    #         embed.add_field(name="Map ID", value=response.json()["games"][0]["mapId"], inline=True)
-    #         embed.add_field(name="Won?", value=response.json()["games"][0]["won"], inline=True)
-    #         embed.add_field(name="Score", value=response.json()["games"][0]["score"], inline=True)
-    #         embed.add_field(name="Assists", value=response.json()["games"][0]["assists"], inline=True)
-    #         embed.add_field(name="Kills", value=response.json()["games"][0]["kills"], inline=True)
-    #         embed.add_field(name="Deaths", value=response.json()["games"][0]["deaths"], inline=True)
-    #         embed.add_field(name="Kill/Death Ratio", value=response.json()["games"][0]["killDeathRatio"], inline=True)
-    #         embed.add_field(name="Headshot Rate", value=response.json()["games"][0]["headshotRate"], inline=True)
-    #         embed.add_field(name="Played at...", value=response.json()["games"][0]["playedAtRecency"], inline=True)
-    #         embed.add_field(name="Headshots", value=response.json()["games"][0]["headshots"], inline=True)
-    #         embed.add_field(name="Medals", value=response.json()["games"][0]["medals"], inline=True)        
+    #         embed.add_field(name="Game Variant", value=response["games"][0]["gameVariant"], inline=True)
+    #         embed.add_field(name="Map ID", value=response["games"][0]["mapId"], inline=True)
+    #         embed.add_field(name="Won?", value=response["games"][0]["won"], inline=True)
+    #         embed.add_field(name="Score", value=response["games"][0]["score"], inline=True)
+    #         embed.add_field(name="Assists", value=response["games"][0]["assists"], inline=True)
+    #         embed.add_field(name="Kills", value=response["games"][0]["kills"], inline=True)
+    #         embed.add_field(name="Deaths", value=response["games"][0]["deaths"], inline=True)
+    #         embed.add_field(name="Kill/Death Ratio", value=response["games"][0]["killDeathRatio"], inline=True)
+    #         embed.add_field(name="Headshot Rate", value=response["games"][0]["headshotRate"], inline=True)
+    #         embed.add_field(name="Played at...", value=response["games"][0]["playedAtRecency"], inline=True)
+    #         embed.add_field(name="Headshots", value=response["games"][0]["headshots"], inline=True)
+    #         embed.add_field(name="Medals", value=response["games"][0]["medals"], inline=True)        
     #         await ctx.send(embed=embed)
 
 def setup(bot):
